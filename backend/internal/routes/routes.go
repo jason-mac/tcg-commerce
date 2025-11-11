af94a330992bd92b7d993255798d220fd7054d81
Jason Mac <jasonvanmac@gmail.com>
2025-11-10 16:50:13 -0800

    [refactor] restructure api

---
+++ backend/internal/routes/routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"net/http"
+	"tcgcommerce.jasonvanmac.com/internal/application"
+	//"github.com/justinas/alice"
+)
+
+func SetRoutes(app *app.Application) http.Handler {
+	// api doc, serve html
+	// "GET /api/v1/"
+
+	// users api
+	// "GET /api/v1/users/signup"
+	// "POST /api/v1/users/signup"
+	// "GET /api/v1/users/login"
+	// "POST /api/v1/users/login"
+	// "POST /api/v1/users/logout"
+
+	return nil
+}
